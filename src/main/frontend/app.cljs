c8cfcda101f8516144bd4d69c183ad13486c9444
nharsch <nigelharsch@gmail.com>
2024-12-15 13:59:36 -0800

    add monaco editor

--- src/main/frontend/app.cljs
+++ src/main/frontend/app.cljs
@@ -1,19 +1,33 @@
 (ns frontend.app
-  (:require [uix.core :refer [defui $]]
+  (:require [uix.core :as uix :refer [defui $]]
             [uix.dom]
             ["@rjsf/core" :default Form]
             ["@rjsf/validator-ajv8" :as validator]
             ["@rjsf/utils" :refer [RJSFSchema]]
+            ["@monaco-editor/react" :default Editor]
             ))
 
 
-(defui button [{:keys [on-click children]}]
-  ($ :button.btn {:on-click on-click}
-    children))
+
+(def default-state {:jsonSchema { :title "test form" :type "string" }
+                    :uiSchema {}
+                    :data {}})
+
 
 (defui app []
-  ($ Form {:schema #js {:title "Test Form" :type "string"}
-           :validator validator}))
+  (let [[state set-state] (uix/use-state default-state)]
+    ($ :<>
+       ($ Form {:schema (clj->js (:jsonSchema state))
+                :validator validator})
+       ($ Editor {:defaultLanguage "JSON"
+                  :defaultValue (-> state
+                                    :jsonSchema
+                                    clj->js
+                                    js/JSON.stringify)
+                  :height "90vh"}))))
+
+
+
 
 (defonce root
   (uix.dom/create-root (js/document.getElementById "root")))
@@ -23,8 +37,8 @@
   (uix.dom/render-root ($ app) root)
   )
 
-;; (init)
 
 (comment
+  (:jsonSchema default-state)
   (+ 1 3 4)
   )
