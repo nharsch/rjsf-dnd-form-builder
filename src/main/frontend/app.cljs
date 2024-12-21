afe444dc4b30ba075626692e653833a88bed6305
nharsch <nigelharsch@gmail.com>
2024-12-20 22:21:36 -0800

    settling on reagent atoms for now

--- src/main/frontend/app.cljs
+++ src/main/frontend/app.cljs
@@ -1,6 +1,8 @@
 (ns frontend.app
   (:require [uix.core :as uix :refer [defui $]]
             [uix.dom]
+            [uix.re-frame :as urf]
+            [reagent.core :as r]
             ["@rjsf/core" :default Form]
             ["@rjsf/validator-ajv8" :as validator]
             ["@rjsf/utils" :refer [RJSFSchema]]
@@ -9,16 +11,21 @@
             ))
 
 
-(def default-state {:jsonSchema { :title "test form" :type "string" }
-                    :uiSchema {}
+(def default-state {:canvas-components []
+                    ;; :jsonSchema { :title "test form" :type "string" }
+                    ;; :uiSchema {}
                     :data {}})
+
+(defonce state (r/atom default-state))
+
+
 
 (defn transform-3D-string [transform]
   (if transform
     (str "translate3d(" (.-x transform) "px, " (.-y transform) "px, 0")))
 
 
-(defui draggable-item [{:keys [id props]}]
+(defui draggable-item [{:keys [id children]}]
   (let [hook-ret (useDraggable #js {:id id})
         attributes (.-attributes hook-ret)
         listeners (.-listeners hook-ret)
@@ -31,39 +38,83 @@
                        }
                       (js->clj listeners)
                       (js->clj attributes))
-       (str "Draggable " id))))
+       children)))
 
 
-(defui droppable [props]
-  (let [hook-ret (useDroppable #js {:id "droppable"})
+(defui droppable [{:keys [id children]}]
+  (let [hook-ret (useDroppable #js {:id id})
         is-over (.-isOver hook-ret)
         set-node-ref (.-setNodeRef hook-ret)
-        style (if is-over
-                {:color "green"})]
+        style {:color (if is-over "green" "black")
+               :height "100px"}]
     ($ :div {:ref set-node-ref
              :style style}
-       (if is-over
-         "Drop here"
-         "Drag here"))))
+       children)))
 
-;; ($ draggable-item {:id "droppable" :props {}})
+
+(defn insert-at
+  ([coll item]
+   (vec (concat coll [item])))
+  ([coll item idx]
+   (vec (concat (take idx coll) [item] (drop idx coll)))))
+
+(comment
+  (insert-at [1 2 3] {:id "text" :type "text"})
+  (insert-at [1 2 3] {:id "text" :type "text"} 2)
+  (insert-at [1 2 3] {:id "text" :type "text"} 4)
+)
+
+(defn add-component-to-canvas
+  ([state component]
+   (add-component-to-canvas state component 0))
+  ([state component drop-target-id]
+   ;; insert component into canvas vector at drop-target-id
+   (let [new-components (insert-at (:canvas-components state) component drop-target-id)]
+     (assoc state :canvas-components new-components))))
+
+
+(comment
+  (add-component-to-canvas default-state {:id "text" :type "text"})
+  (-> default-state
+      (add-component-to-canvas {:id "text" :type "text"} 0)
+      (add-component-to-canvas {:id "text" :type "text"} 0))
+  (add-component-to-canvas @state {:id "text" :type "text"} 0)
+  (swap! state add-component-to-canvas {:id "text" :type "text"} 0)
+  (:canvas-components @state)
+  )
+
+(defn onDragEnd [event]
+  ;; (js/console.log "onDragEnd" event)
+  (js/console.log (.. event -active -id))
+  (swap! state add-component-to-canvas {:id (rand-int 10) :type (.. event -active -id)} 0)
+  (println "state" (:canvas-components @state))
+  )
+
 
 (defui app []
   ;; TODO: use atoms for state instead
-  ($ :<>
-     ($ :h1 "JSON Schema Form Builder")
-     ($ DndContext {:handle-drag-end #(js/console.log "drag end")}
-        ($ draggable-item {:id "1" :props {}})
-        ($ droppable {})
-        )
-     ;; ($ Form {:schema (clj->js (:jsonSchema state)) :validator validator})
-     ;; ($ Editor {:defaultLanguage "JSON"
-     ;;            :defaultValue (-> state
-     ;;                              :jsonSchema
-     ;;                              clj->js
-     ;;                              js/JSON.stringify)
-     ;;            :height "90vh"})
-     )
+  (let [state (urf/use-reaction state)]
+       ($ :<>
+          ($ :h1 "JSON Schema Form Builder")
+          ($ DndContext {:onDragEnd onDragEnd}
+             ($ draggable-item {:id "text"} "text input")
+             ($ droppable
+                {:id "droppable"}
+                (map-indexed
+                 (fn [idx component]
+                   ($ :li {:id (:id component) :key idx} (:type component)))
+                 (:canvas-components state)
+                 )
+                )
+             )
+          ;; ($ Form {:schema (clj->js (:jsonSchema state)) :validator validator})
+          ;; ($ Editor {:defaultLanguage "JSON"
+          ;;            :defaultValue (-> state
+          ;;                              :jsonSchema
+          ;;                              clj->js
+          ;;                              js/JSON.stringify)
+          ;;            :height "90vh"})
+          ))
   )
 
 
@@ -71,7 +122,7 @@
   (uix.dom/create-root (js/document.getElementById "root")))
 
 
-(defn start []
+(defn ^:dev/after-load start []
   (uix.dom/render-root ($ app) root)
   )
 
